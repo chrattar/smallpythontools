9deb95c6f97336c309d509fd2134e010be7c1740
Christopher Attar <cattar@protonmail.ch>
2025-03-01 13:51:11 -0500

    Update gross-margin-dash.py

--- gross-margin-dash.py
+++ gross-margin-dash.py
@@ -1,24 +1,20 @@
 from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import pandas as pd
-
-# Load your data
 df = pd.read_csv('gmpdata.csv')
 
-# Convert 'Total Revenue' and 'Total Cost' to numeric, removing non-numeric characters like '$' and commas
+#Covn to num from str
 df['Total Revenue'] = pd.to_numeric(df['Total Revenue'].replace('[\$,]', '', regex=True), errors='coerce')
 df['Total Cost'] = pd.to_numeric(df['Total Cost'].replace('[\$,]', '', regex=True), errors='coerce')
 
-# Calculate Gross Margin % and Gross Profit %
+# GM & GP calc
 df['Gross Margin %'] = (df['Total Revenue'] - df['Total Cost']) / df['Total Revenue'] * 100
-df['Gross Profit %'] = df['Gross Margin %']  # Assuming it's calculated similarly
+df['Gross Profit %'] = df['Gross Margin %']
+df['Period'] = pd.Categorical(df['Period']) #Send acct period to category for group
 
-# Ensure 'Period' is treated as a categorical type that can work with the slider
-df['Period'] = pd.Categorical(df['Period'])
-
-# Create a consistent color map for 'TopGrp'
+# Color maps
 unique_groups = df['TopGrp'].unique()
-colors = px.colors.qualitative.Plotly  # or any other color palette
+colors = px.colors.qualitative.Plotly
 color_discrete_map = {group: colors[i % len(colors)] for i, group in enumerate(unique_groups)}
 
 app = Dash(__name__)
@@ -44,14 +40,14 @@ def update_figures(selected_period_index):
     selected_period = df['Period'].cat.categories[selected_period_index]
     filtered_df = df[df.Period == selected_period]
 
-    # First graph for Total Revenue and Gross Margin %
+    #REV $ vs GP
     fig1 = px.scatter(filtered_df, x="Total Revenue", y="Gross Margin %",
                       size="Total Revenue", color="TopGrp", hover_name="TopGrp",
                       size_max=60, log_x=True, color_discrete_map=color_discrete_map)
 
     fig1.update_layout(transition_duration=500)
 
-    # Second graph for Gross Profit % over months
+    # GP over Mths
     fig2 = px.scatter(df, x='Period', y='Gross Profit %', color='TopGrp',
                       labels={"Period": "Month", "Gross Profit %": "Gross Profit Percentage"},
                       color_discrete_map=color_discrete_map)
