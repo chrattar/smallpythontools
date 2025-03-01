99cf99b0380d8b5ccc12984165d2e0b027c874f0
Christopher Attar <cattar@protonmail.ch>
2025-03-01 13:52:43 -0500

    Update img_converter.py

--- img_converter.py
+++ img_converter.py
@@ -2,23 +2,18 @@ from PIL import Image
 import os
 
 def convert_webp_to_png(input_folder,  output_folder):
-    #Ensure output folder exists
     os.makedirs(output_folder, exist_ok=True)
-    #List all files in the input folder
-    files = os.listdir(input_folder)
+    files = os.listdir(input_folder) # LIST FILES IN DIR
 
     for filename in files:
         if filename.endswith('.webp'):
-            #Construct the input and output file paths
             input_path = os.path.join(input_folder, filename)
             output_path = os.path.join(output_folder, os.path.splitext(filename)[0] + '.png')
-
-            #Open Webp Image and Save as PNG
             img = Image.open(input_path)
-            img.save(output_path, 'PNG')
+            img.save(output_path, 'PNG') 
 
 if __name__ == "__main__":
-    input_folder = 'C:/Codex/ImageFolder/DallE' # Replace with your input folder 
-    output_folder = 'C:/Codex/ImageFolder/DallE_png' #Replace with your output folder
+    input_folder = '/input_dir'
+    output_folder = '/output_dir'
 
 convert_webp_to_png(input_folder, output_folder)
